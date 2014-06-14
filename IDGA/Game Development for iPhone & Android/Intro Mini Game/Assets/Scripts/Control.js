6b0569c7e020a032325a9c4cb66fd5a04562056e
Kappy0 <ryanklaproth@gmail.com>
2014-06-14 13:16:49 -0500

    Intro to Mini Game Folder First Pass
    First half of Mini Game project

---
+++ IDGA/Game Development for iPhone & Android/Intro Mini Game/Assets/Scripts/Control.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {}
+
+function Update () 
+{
+	//transform.position.x += 0.2;
+	transform.position.x += 1 * Time.deltaTime;
+}
---
+++ IDGA/Game Development for iPhone & Android/Intro Mini Game/Assets/Scripts/Mouse.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {}
+
+function Update () 
+{
+	transform.position.x = (Input.mousePosition.x - (Screen.width / 2)) / (Screen.width / 2);
+	Debug.Log(Input.mousePosition.x);
+}
