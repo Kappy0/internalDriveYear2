113eb3f7505b52a544604e061b46e96a3953ec2c
Kappy0 <ryanklaproth@gmail.com>
2014-06-14 00:37:32 -0500

    Javascript Folder
    Demos

---
+++ IDPA/Programming Labs 101/Javascript/Classes and Functions/cat.js
@@ -0,0 +1,27 @@
+var meowCount;
+
+function Cat(name, age)
+{
+	this.name = name;
+	this.age = age;
+	
+	this.meow = function(numOfTimes)
+	{
+		var x = "";
+		
+		for(var i = 0; i < numOfTimes; i++)
+		{
+			x += "Meow!" + "<br>";
+			document.getElementById("meow").innerHTML = x;
+		}
+	}
+}
+
+function Meow()
+{
+	meowCount = parseInt(prompt("How many times will the cat meow?", "Please enter here"));
+	ginger.meow(meowCount);
+}
+
+var ginger = new Cat("Ginger", 10);
+
