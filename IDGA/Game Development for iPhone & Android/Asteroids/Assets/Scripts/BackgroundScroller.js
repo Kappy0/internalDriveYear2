fac1ed44ec8b9dd73a132de6bdd6c7adae4068f7
Kappy0 <ryanklaproth@gmail.com>
2014-06-15 21:07:18 -0500

    Background
    Asteroids Adding background (and parallax scrolling)

---
+++ IDGA/Game Development for iPhone & Android/Asteroids/Assets/Scripts/BackgroundScroller.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var scrollSpeed: float = 0;
+
+function Start () {}
+
+function Update () 
+{
+	renderer.material.SetTextureOffset("_MainTex", Vector2(0, Time.time * scrollSpeed));
+}
