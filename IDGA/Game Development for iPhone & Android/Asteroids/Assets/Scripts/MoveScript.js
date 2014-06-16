60be2e9d02331d05dc32d7d5afed9b93de71fd39
Kappy0 <ryanklaproth@gmail.com>
2014-06-16 17:31:18 -0500

    Delayed Respawn Challenge
    Asteroids project

--- IDGA/Game Development for iPhone & Android/Asteroids/Assets/Scripts/MoveScript.js
+++ IDGA/Game Development for iPhone & Android/Asteroids/Assets/Scripts/MoveScript.js
@@ -2,6 +2,10 @@
 
 var playerSpeed: float = 0.0;
 
+var timer: float = 0.0;
+
+var invincible: boolean = true;
+
 function Start () {}
 
 function Update () 
@@ -11,12 +15,22 @@ function Update ()
 	
 	transform.position.x = Mathf.Clamp(transform.position.x, -4, 4);
 	transform.position.z = Mathf.Clamp(transform.position.z, -1.7, 1.7);
+	
+	timer += Time.deltaTime;
+	
+	if(timer > 5.0 && invincible)
+	{
+		invincible = false;
+	}
 }
 
-function OnCollisionEnter(deadShip : Collision)
+function OnTriggerEnter(deadShip : Collider)
 {
-	if(deadShip.gameObject.tag == "asteroid")
+	if(deadShip.gameObject.tag == "asteroid" && !invincible)
 	{
 		Destroy(gameObject);
+		timer = 0.0;
 	}
+	
+	
 }
---
+++ IDGA/Game Development for iPhone & Android/Asteroids/Assets/Scripts/PlayerRespawn.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+var player: GameObject;
+var newPlayer: GameObject;
+
+var respawnPos: Transform;
+
+var timer: float = 0.0;
+
+var invincible: boolean = true;
+
+function Start () {}
+
+function Update () 
+{	
+	if(player != null)
+	{
+		transform.position = player.transform.position;
+		timer += Time.deltaTime;
+		
+		if(timer > 5.0 && invincible)
+		{
+			invincible = false;
+		}
+	}
+}
+
+function OnTriggerEnter(gameObj : Collider)
+{
+	if(gameObj.gameObject.tag == "asteroid" && !invincible)
+	{
+		invincible = true;
+		Invoke("RespawnPlayer", 2);
+	}
+}
+
+function RespawnPlayer()
+{
+	var instance: GameObject = Instantiate(newPlayer);
+	instance.transform.position = respawnPos.position;
+	player = instance;
+	timer = 0.0;
+}
