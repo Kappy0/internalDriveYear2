4bccc5e4b3905e30433bcbf6c50373566035fa8d
Kappy0 <ryanklaproth@gmail.com>
2014-06-15 20:41:01 -0500

    Title Screen
    Asteroids Project

---
+++ IDGA/Game Development for iPhone & Android/Asteroids/Assets/Scripts/TitleGUI.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var MySkin: GUISkin;
+
+function Start () {}
+
+function Update() {}
+
+function OnGUI()
+{
+	var buttonW: int = 100;
+	var buttonH: int = 50;
+
+	var halfScreenW: float = Screen.width / 2;
+	var halfButtonW: float = buttonW / 2;
+
+	if(GUI.Button(Rect(halfScreenW - halfButtonW, 560, buttonW, buttonH), "Start"))
+	{
+		print("You clicked me!");
+		//Application.LoadLevel("Game");
+	}
+}
