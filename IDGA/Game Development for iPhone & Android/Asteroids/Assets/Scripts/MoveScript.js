1cf35cfc749f8b4980d1931bbe8e1b27dc000598
Kappy0 <ryanklaproth@gmail.com>
2014-06-17 01:02:40 -0500

    Thruster Challenge
    Asteroids Project.

--- IDGA/Game Development for iPhone & Android/Asteroids/Assets/Scripts/MoveScript.js
+++ IDGA/Game Development for iPhone & Android/Asteroids/Assets/Scripts/MoveScript.js
@@ -6,12 +6,19 @@ var timer: float = 0.0;
 
 var invincible: boolean = true;
 
+var thruster: Transform;
+
 function Start () {}
 
 function Update () 
 {
 	transform.Translate((Input.GetAxisRaw("Horizontal") * Time.deltaTime * playerSpeed * -1), 0, 
 							(Input.GetAxisRaw("Vertical") * Time.deltaTime * playerSpeed * -1));
+							
+	if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+	{
+		Instantiate(thruster, transform.position, Quaternion.Euler(0, 0, -90));
+	}
 	
 	transform.position.x = Mathf.Clamp(transform.position.x, -4, 4);
 	transform.position.z = Mathf.Clamp(transform.position.z, -1.7, 1.7);
