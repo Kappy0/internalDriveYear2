8f602629e811d9b337bf8a2d1c24969fb5dde864
Kappy0 <ryanklaproth@gmail.com>
2014-06-16 23:20:42 -0500

    Lasers
    Asteroids Project

---
+++ IDGA/Game Development for iPhone & Android/Asteroids/Assets/Scripts/LaserScript.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+var laserSpeed : float = 0;
+
+function Start () {}
+
+function Update () 
+{
+	transform.Translate(0, 0, laserSpeed * Time.deltaTime);
+	
+	if(transform.position.z > 3)
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ IDGA/Game Development for iPhone & Android/Asteroids/Assets/Scripts/ShootScript.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var laser: GameObject;
+
+var lastShot: float = 0;
+var fireRate: float = 0;
+
+function Start () {}
+
+function Update () 
+{
+//	if(Input.GetButtonDown("Fire1"))
+//	{
+//		Fire();
+//	}
+	
+	if(Input.GetButtonDown("Fire1") && (Time.time > lastShot + fireRate)) //Fire1 = Spacebar or Ctrl
+	{
+		lastShot = Time.time;
+		Fire();
+	}
+}
+
+function Fire()
+{
+	Instantiate(laser, transform.position, Quaternion.Euler(0, 0, -90));
+}
