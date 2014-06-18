7b3a0a56864abd94b17a8af8ec992ad496be1df8
Kappy0 <ryanklaproth@gmail.com>
2014-06-18 14:02:44 -0500

    Unity Enumerations
    Advanced Scripting

---
+++ IDGA/Game Development for iPhone & Android/Advanced Scripting/Assets/Enumerations/RPSGame.js
@@ -0,0 +1,86 @@
+﻿#pragma strict
+
+var PlayerMove : HandSignal;
+var aiMove : HandSignal;
+
+var myString : String;
+var aiString : String;
+
+var aiChoose : boolean = false;
+
+enum HandSignal
+{
+	Rock,
+	Paper,
+	Scissors
+};
+
+function Start () {}
+
+function Update () 
+{
+	PlayerInput();
+	
+	if(aiChoose)
+	{
+		inputAI();
+	}
+	
+	myString = PlayerMove.ToString();
+	aiString = aiMove.ToString();
+	
+	CheckScore(playerMove, aiMove);
+}
+
+function OnGUI()
+{
+	GUI.Label (Rect (10, 10, 100, 20), "Player: " + myString);
+	GUI.Label (Rect (10, 30, 100, 20), "AI: " + aiString);
+}
+
+function PlayerInput()
+{	
+	if(Input.GetKeyDown(KeyCode.Alpha1))
+	{   //press 1 for Rock
+		PlayerMove = HandSignal.Rock;
+		aiChoose = true;
+    }
+    
+    if(Input.GetKeyDown(KeyCode.Alpha2))
+    {   //press 2 for Paper
+        PlayerMove = HandSignal.Paper;
+        aiChoose = true;
+    }
+    
+    if(Input.GetKeyDown(KeyCode.Alpha3))
+    {   //press 3 for Scissor
+        PlayerMove = HandSignal.Scissors;
+        aiChoose = true;
+    }
+    
+    return PlayerMove;
+}
+
+function inputAI()
+{
+	var choice : int = Random.Range(0, 3);
+	
+	if(choice == 0)
+	{ 
+		aiMove = HandSignal.Rock;
+    }
+    
+    if(choice == 1)
+    {  
+        aiMove = HandSignal.Paper;
+    }
+    
+    if(choice == 2)
+    {   
+        aiMove = HandSignal.Scissors;
+    }
+    
+    aiChoose = false;
+    
+    return aiMove;
+}
