b9f0ce72d082fb3b1be37efcac6e6d8aeb30928f
Kappy0 <ryanklaproth@gmail.com>
2014-06-18 14:54:04 -0500

    Unity Coroutines
    Advanced Scripting.

---
+++ IDGA/Game Development for iPhone & Android/Advanced Scripting/Assets/Coroutines/PrintTime.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+function Start () 
+{
+	print("SECONDS COROUTINE: " + Time.time);
+	
+	yield WaitForSeconds(5);
+	
+	print("SECONDS COROUTINE: " + Time.time);
+}
+
+function Update () {}
+
+function FixedUpdate()
+{
+	PrintTime();
+}
+
+function PrintTime()
+{
+	print("FIXEDUPDATE COROUTINE: " + Time.time);
+	
+	yield new WaitForFixedUpdate();
+	
+	print("FIXEDUPDATE COROUTINE: " + Time.time);
+}
