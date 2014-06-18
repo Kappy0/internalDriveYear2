5bfe9f64e6715cc9e0fa6ca12a2e4bfa79575bc7
Kappy0 <ryanklaproth@gmail.com>
2014-06-18 14:44:39 -0500

    Unity Calls and Invoke
    Advanced Scripting

---
+++ IDGA/Game Development for iPhone & Android/Advanced Scripting/Assets/Calls and Invoke/CreateObject.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var target : GameObject;
+
+function Start () 
+{
+	Invoke("SpawnObject", 2);
+}
+
+function Update () {}
+
+function SpawnObject()
+{
+	Instantiate(target, new Vector3(0, 2, 0), Quaternion.identity);
+}
+
+function SpawnObjectRand()
+{
+	Instantiate(target, new Vector3(Random.Range(0, 10), Random.Range(2, 10), Random.Range(0, 10)), Quaternion.identity);
+}
---
+++ IDGA/Game Development for iPhone & Android/Advanced Scripting/Assets/Calls and Invoke/ReferenceCreateObject.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var script : CreateObject;
+
+function Start () 
+{
+	script.InvokeRepeating("SpawnObjectRand", 2, 1);
+}
+
+function Update () {}
