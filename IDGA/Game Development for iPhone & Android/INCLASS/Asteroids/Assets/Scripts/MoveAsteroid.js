db16234b89c60b4c6da4dfd29147482c200365cd
Ryan Klaproth <ryanklaproth@gmail.com>
2014-07-09 11:37:38 -0500

    Asteroids INCLASS
    Up to adding asteroids

---
+++ IDGA/Game Development for iPhone & Android/INCLASS/Asteroids/Assets/Scripts/MoveAsteroid.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var velocity : float = 0.0f;
+var euler : Vector3 = Vector3(0, 100, 0);
+var rotation : Quaternion = Quaternion.identity;
+
+function Start () {}
+
+function Update () {}
+
+function FixedUpdate()
+{
+	var deltaRotation : Quaternion = Quaternion.Euler(euler * Time.deltaTime);
+	rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+	rigidbody.AddForce(0, 0, velocity);
+}
---
+++ IDGA/Game Development for iPhone & Android/INCLASS/Asteroids/Assets/Scripts/SpawnScript.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var asteroid : Rigidbody;
+var spawnPos : Transform;
+
+var isSpawning : boolean = false;
+var timer : float = 0.0f;
+
+function Start () {}
+
+function Update () 
+{
+	if(!isSpawning)
+	{
+		timer += Time.deltaTime;
+	}
+	
+	if(timer >= 2)
+	{
+		Spawn();
+	}
+}
+
+function Spawn()
+{
+	isSpawning = true;
+	var newAsteroid : Rigidbody = Instantiate(asteroid, spawnPos.position, spawnPos.rotation);
+	isSpawning = false;
+	timer = 0;
+}
