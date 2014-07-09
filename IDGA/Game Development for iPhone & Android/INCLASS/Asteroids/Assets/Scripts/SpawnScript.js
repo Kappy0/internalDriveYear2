634c203317c2f1e8ecd761d20f54ed9b6348b0f7
Ryan Klaproth <ryanklaproth@gmail.com>
2014-07-09 14:57:30 -0500

    INCLASS Asteroids
    Up to Spawn Randomization

--- IDGA/Game Development for iPhone & Android/INCLASS/Asteroids/Assets/Scripts/SpawnScript.js
+++ IDGA/Game Development for iPhone & Android/INCLASS/Asteroids/Assets/Scripts/SpawnScript.js
@@ -1,10 +1,13 @@
 ﻿#pragma strict
 
 var asteroid : Rigidbody;
-var spawnPos : Transform;
+//var spawnPos : Transform [];
 
 var isSpawning : boolean = false;
 var timer : float = 0.0f;
+
+//var totalSpawns : int = 4; //Size of Transform array
+//var currentSpawn : int = 0; //Index iterator through Transform array
 
 function Start () {}
 
@@ -24,7 +27,21 @@ function Update ()
 function Spawn()
 {
 	isSpawning = true;
-	var newAsteroid : Rigidbody = Instantiate(asteroid, spawnPos.position, spawnPos.rotation);
+	//var newAsteroid : Rigidbody = Instantiate(asteroid, spawnPos[currentSpawn].position, spawnPos[currentSpawn].rotation);
+	
+	for(var i : int = 0; i < 4; i++)
+	{
+		var pos : Vector3 = Vector3(Random.Range(-4.2, 4.2), 0, 6.5);
+		var newAsteroid : Rigidbody = Instantiate(asteroid, pos, Quaternion.identity);
+	}
 	isSpawning = false;
+	
+//	currentSpawn += 1;
+//	
+//	if(currentSpawn == totalSpawns)
+//	{
+//		currentSpawn = 0;
+//	}
+	
 	timer = 0;
 }
