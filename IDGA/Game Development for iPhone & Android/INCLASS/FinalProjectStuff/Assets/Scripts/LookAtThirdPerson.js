0ba307e9a448c958521abb981f729e39bf422506
Ryan Klaproth <ryanklaproth@gmail.com>
2014-07-14 11:29:48 -0500

    INCLASS Final Project stuff
    Third person cam + collision stuff

---
+++ IDGA/Game Development for iPhone & Android/INCLASS/FinalProjectStuff/Assets/Scripts/LookAtThirdPerson.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var height = 1.0;
+var distance = 15.0;
+var target : Transform;
+
+function Start () {}
+
+ 
+function Update () 
+{
+	transform.position = target.position;
+    transform.position.y += height;
+    transform.position.z -= distance;
+    transform.LookAt(target.position);
+}
---
+++ IDGA/Game Development for iPhone & Android/INCLASS/FinalProjectStuff/Assets/Scripts/Move.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var speed : float = 0.0;
+
+function Start () {}
+
+function Update () 
+{
+	transform.Translate((Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed), 0, 
+						(Input.GetAxisRaw("Vertical") * Time.deltaTime * speed));
+}
